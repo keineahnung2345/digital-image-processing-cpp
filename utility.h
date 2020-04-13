c85254f7abd33c95baf28010860b19fc9b659a7f
keineahnung2345 <mimifasosofamire1123@gmail.com>
2020-04-13 15:02:30 +0800

    ConcatHorizontal, to_string_with_precision

--- utility.h
+++ utility.h
@@ -31,15 +31,16 @@ std::string type2str(int type) {
   return r;
 };
 
-void Show(cv::Mat& img, string title = "Display Window"){
-    cv::namedWindow( title, cv::WINDOW_AUTOSIZE);// Create a window for display.
-    cv::imshow(title, img);                   // Show our image inside it.
-    cv::waitKey(0);
-    cv::destroyAllWindows();
+void stringReplace(string& base, string from, string to){
+    //start search from last end to speed up
+    size_t pos = 0;
+    while((pos = base.find(from, pos)) != string::npos){
+        base.replace(pos, from.length(), to);
+    }
 };
 
-void ShowHorizontal(vector<cv::Mat>& imgs, string title = "Display Window"){
-    cv::Mat target = imgs[0];
+void ConcatHorizontal(vector<cv::Mat>& imgs, cv::Mat& target){
+    target = imgs[0];
     //assume imgs[0]'s height is always largest
     int finalHeight = target.rows;
     for(int i = 1; i < imgs.size(); i++){
@@ -52,16 +53,33 @@ void ShowHorizontal(vector<cv::Mat>& imgs, string title = "Display Window"){
         }
         hconcat(target, imgs[i], target);
     }
+};
 
-    // cv::namedWindow( title, cv::WINDOW_AUTOSIZE);// Create a window for display.
-    // cv::imshow(title, img2);                   // Show our image inside it.
-    // cv::waitKey(0);
-    // cv::destroyAllWindows();
+void Show(cv::Mat& img, string title = "Display Window", bool save = false){
+    if(save){
+        stringReplace(title, " ", "_");
+        cv::imwrite("images/result/" + title + ".png", img);
+    }else{
+        cv::namedWindow( title, cv::WINDOW_AUTOSIZE);// Create a window for display.
+        cv::imshow(title, img);                   // Show our image inside it.
+        cv::waitKey(0);
+        cv::destroyAllWindows();
+    }
+};
 
-    cv::namedWindow( title, cv::WINDOW_AUTOSIZE);// Create a window for display.
-    cv::imshow(title, target);                   // Show our image inside it.
-    cv::waitKey(0);
-    cv::destroyAllWindows();
+void ShowHorizontal(vector<cv::Mat>& imgs, string title = "Display Window", bool save = false){
+    cv::Mat target;
+    ConcatHorizontal(imgs, target);
+
+    if(save){
+        stringReplace(title, " ", "_");
+        cv::imwrite("images/result/" + title + ".png", target);
+    }else{
+        cv::namedWindow( title, cv::WINDOW_AUTOSIZE);// Create a window for display.
+        cv::imshow(title, target);                   // Show our image inside it.
+        cv::waitKey(0);
+        cv::destroyAllWindows();
+    }
 };
 
 void pad(cv::Mat& img, int padt, int padb, int padl, int padr, int mode = cv::BORDER_CONSTANT){
@@ -73,4 +91,13 @@ void pad(cv::Mat& img, int padt, int padb, int padl, int padr, int mode = cv::BO
     else
         copyMakeBorder(img, tmp, padt, padb, padl, padr, mode);
     img = tmp;
+};
+
+template <typename T>
+std::string to_string_with_precision(const T a_value, const int n = 6)
+{
+    std::ostringstream out;
+    out.precision(n);
+    out << std::fixed << a_value;
+    return out.str();
 };
